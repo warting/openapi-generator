20b18ad82803ca801dfc664f45b4be0c224d2b5e
William Cheng <wing328hk@gmail.com>
2026-02-25 16:26:01 +0800

    update python samples

--- samples/openapi3/client/petstore/python-lazyImports/petstore_api/models/uuid_with_pattern.py
+++ samples/openapi3/client/petstore/python-lazyImports/petstore_api/models/uuid_with_pattern.py
@@ -42,7 +42,8 @@ class UuidWithPattern(BaseModel):
         return value
 
     model_config = ConfigDict(
-        populate_by_name=True,
+        validate_by_name=True,
+        validate_by_alias=True,
         validate_assignment=True,
         protected_namespaces=(),
     )
